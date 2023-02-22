6be0e2692af69f1d3f989b89ef9242d174692c8f
jmcavaddy <jakemcavaddy@gmail.com>
2023-02-22 09:04:06 -0500

    Cleaned up script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,39 +13,42 @@ $(function () {
     localStorage.setItem(this.parentElement.id, text);
   });
 
-    // This function will get the current hour and compare it to the id of each time block. If the id is less than the current hour,
-    // it will add the class of past to the time block. If the id is equal to the current hour, it will add the class of present. 
-    // If the id is greater than the current hour, it will add the class of future.
-    function timeTracker() {
-      var currentHour = dayjs().hour();
-      $(".time-block").each(function () {
-        // blockHour grabs the hour by splitting the id at the dash and grabbing the number from the array.
-        // The number is then parsed into an integer for comparison to current hour.
-        var blockHour = parseInt($(this).attr("id").split("-")[1]);
-        if (blockHour < currentHour) {
-          $(this).addClass("past");
-        } else if (blockHour === currentHour) {
-          $(this).removeClass("past");
-          $(this).addClass("present");
-        } else {
-          $(this).removeClass("past");
-          $(this).removeClass("present");
-          $(this).addClass("future");
-        }
-      });
-    }
-    timeTracker();
-
-    // This function will get the text from local storage and set it to the text area with the same id as the key.
-    function getLocalStorage() {
-      for (var i = 9; i < 18; i++) {
-        var text = localStorage.getItem("hour-" + i);
-        $("#hour-" + i).children(".description").val(text);
+  // This function will get the current hour and compare it to the id of each time block. If the id is less than the current hour,
+  // it will add the class of past to the time block. If the id is equal to the current hour, it will add the class of present. 
+  // If the id is greater than the current hour, it will add the class of future.
+  
+  function updateColor() {
+    console.log("updateColor")
+    var currentHour = dayjs().hour();
+    $(".time-block").each(function () {
+      // blockHour grabs the hour by splitting the id at the dash and grabbing the number from the array.
+      // The number is then parsed into an integer for comparison to current hour.
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      if (blockHour < currentHour) {
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
       }
-    }
-    getLocalStorage();
-    
-    // Display the current date in the header of the page.
-    $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+    });
+  }
+  updateColor();
 
+  // This function will get the text from local storage and set it to the text area with the same id as the key.
+  function getLocalStorage() {
+    for (var i = 9; i < 18; i++) {
+      var text = localStorage.getItem("hour-" + i);
+      $("#hour-" + i).children(".description").val(text);
+    }
+  }
+  getLocalStorage();
+    
+  // Display the current date in the header of the page.
+    $("#currentDay").text(dayjs().format("dddd, MMMM D"));
   });
+
